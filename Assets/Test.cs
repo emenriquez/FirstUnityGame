0b0a818fa69d930f743edf59a50eea2e38b2a0da
Erik <uss593@my.utsa.edu>
2020-07-12 13:40:48 -0500

    added player movement and fixed camera to player

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -5,6 +5,9 @@ public class Test : MonoBehaviour
 
     public Rigidbody rb;
 
+    public float forwardForce = 200f;
+    public float sidewaysForce = 500f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 500 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") ) {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if ( Input.GetKey("a") ) {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
     }
 }
