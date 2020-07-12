93ea9900a2bede76249610ccbb437e82cf95fe9d
Erik <uss593@my.utsa.edu>
2020-07-12 13:03:56 -0500

    Fixed perspective and ground

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -13,6 +13,6 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(-500 * Time.deltaTime, 200 * Time.deltaTime, 0);
+        rb.AddForce(0, 0, 500 * Time.deltaTime);
     }
 }
