acac10e3481c4f435200504c22e2aa2514bd1d8a
Erik <uss593@my.utsa.edu>
2020-07-12 12:37:37 -0500

    First Commit

---
+++ Assets/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(-500 * Time.deltaTime, 200 * Time.deltaTime, 0);
+    }
+}
